17da28acdabceea41506716f128ffe66308e3382
billzagoras <84288441+billzagoras@users.noreply.github.com>
2023-06-09 04:04:03 +0300

    Update service.go

--- internal/app/ptlist/service.go
+++ internal/app/ptlist/service.go
@@ -27,40 +27,49 @@ func (s *Service) GetPtList(ctx context.Context, period, tz, t1, t2 string) (*Pt
 	timeNowInLocation := time.Now().In(loc)
 
 	// Get the offset in seconds for each time zone
-	//for the time zones
+	// for the time zones
 	_, currentTimeLocationOffset := timeNowInLocation.Zone()
 
-	timeObj1, err := time.ParseInLocation("20060102T150405Z", t1, loc)
+	// UTC t1
+	timeObj1UTC, err := time.Parse("20060102T150405Z", t1)
 	if utils.CheckErr(err) {
 		return nil, errors.GetError(errors.TimeParsingError)
 	}
 
-	timeObj2, err := time.ParseInLocation("20060102T150405Z", t2, loc)
+	// UTC t2
+	timeObj2UTC, err := time.Parse("20060102T150405Z", t2)
 	if utils.CheckErr(err) {
 		return nil, errors.GetError(errors.TimeParsingError)
 	}
 
-	errResp := utils.Round(&timeObj1, &timeObj2, period)
+	errResp := utils.Round(&timeObj1UTC, &timeObj2UTC, period)
 	if utils.CheckErr(errResp) {
 		return nil, errResp
 	}
 
+	// Local t1
+	timeObj1Local := timeObj1UTC.In(loc)
+	_, timeObj1UTC = utils.NormalizeTime(timeObj1Local, timeObj1UTC)
+
+	// Local t2
+	timeObj2Local := timeObj2UTC.In(loc)
+	_, timeObj2UTC = utils.NormalizeTime(timeObj2Local, timeObj2UTC)
+
 	timestamps := []string{}
-	for !timeObj1.After(timeObj2) {
+	for !timeObj1UTC.After(timeObj2UTC) {
 		// Get the offset in seconds for each time zone for the time zones.
-		_, givenTimeLocationOffset := timeObj1.Zone()
+		_, givenTimeLocationOffset := timeObj1UTC.In(loc).Zone()
 		duration := (currentTimeLocationOffset - givenTimeLocationOffset) / 60 / 60
 		if duration != 0 {
 			currentTimeLocationOffset = givenTimeLocationOffset
-			timeObj1 = timeObj1.Add(time.Duration(duration) * time.Hour)
+			timeObj1UTC = timeObj1UTC.Add(time.Duration(duration) * time.Hour)
 
-			// Monkey business :p
-			timeObj2 = timeObj2.Add(time.Duration(duration) * time.Hour)
+			timeObj2UTC = timeObj2UTC.Add(time.Duration(duration) * time.Hour)
 		}
 
-		timestamps = append(timestamps, timeObj1.Format("20060102T150405Z"))
+		timestamps = append(timestamps, timeObj1UTC.Format("20060102T150405Z"))
 
-		errResp := utils.AddPeriod(&timeObj1, period)
+		errResp := utils.AddPeriod(&timeObj1UTC, period)
 		if utils.CheckErr(err) {
 			return nil, errResp
 		}
